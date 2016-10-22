53ede3861ba7c22d578254bc48001cfb91ad4612
Daniel Thorngren <dpthorngren@gmail.com>
2016-10-22 12:36:07 -0700

    Some basic structure of sam and the samplers.

--- sam.cpp
+++ sam.cpp
@@ -1,5 +1,37 @@
-#include <iostream>
+#include "sam.h"
 
-double testFunction(double x){
-    return 2*x;
+std::string BaseSampler::getStatus(){
+    std::string name = this->name;
+    if(name.empty()){
+        name = std::string("AnonymousSampler");
+    }
+    return name + std::string(": No further information implemented.");
+}
+
+Sam::Sam(size_t nDim, double (*logLike)(double*, size_t)){
+    this->nDim = nDim;
+    this->logLike = logLike;
+    return;
+}
+
+Sam::~Sam(){
+    for(size_t i=0; i<this->samplers.size(); i++){
+        if(samplers[i] != NULL){
+            delete samplers[i];
+            samplers[i] = NULL;
+        }
+    }
+    return;
+}
+
+std::string Sam::getStatus(){
+    std::stringstream status;
+    status << "===== Sam Information =====" << std::endl;
+    status << "Dimensions: " << this->nDim << std::endl;
+    status << "Number of samplers:" << this->samplers.size() << std::endl;
+    status << "===== Sampler Information =====" << std::endl;
+    for(size_t i=0; i<this->samplers.size(); i++){
+        status << samplers[i]->getStatus();
+    }
+    return std::string(status.str());
 }
--- sam.h
+++ sam.h
@@ -1,6 +1,29 @@
 #ifndef sam_h__
 #define sam_h__
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <cmath>
 
-double testFunction(double);
+class BaseSampler{
+public:
+    std::string name;
+    std::string getStatus();
+    void sample();
+};
+
+class Sam{
+public:
+    // Parameters
+    size_t nDim;
+    double (*logLike)(double*, size_t);
+    std::vector<BaseSampler*> samplers;
+
+    // User-called Functions
+    Sam(size_t, double (*)(double*,size_t));
+    ~Sam();
+    std::string getStatus();
+};
 
 #endif // sam_h__
